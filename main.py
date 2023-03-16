073d04bfbb8833b83e9c5d0cb8649e380aca3457
Emilllo <Emily.Ostashevsky@gmail.com>
2023-03-16 20:33:38 +0200

    Code cleanup

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-# python3
+# 221RDB231 Emīlija Ostaševska 4.gr
+
 
 def parallel_processing(n, m, data):
-
+    
     output_pairs = []
     times = [0] * n
     
@@ -13,30 +14,21 @@ def parallel_processing(n, m, data):
 
     return output_pairs
 
+
 def main():
-    # TODO: create input from keyboard
-    # input consists of two lines
-    # first line - n and m
-    # n - thread count 
-    # m - job count
+
     n = 0
     m = 0
 
     arr = list(map(int, input().split()))
-
     n = arr[0]
     m = arr[1]
 
-    # second line - data 
-    # data - contains m integers t(i) - the times in seconds it takes any thread to process i-th job
     data = list(map(int, input().split()))
-
     assert len(data) == m
 
-    # TODO: create the function
     result = parallel_processing(n, m, data)
-    
-    # TODO: print out the results, each pair in it's own line
+
     for i, j in result:
         print(i, j)
 
